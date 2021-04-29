d61dfdba8651f9acfcca47d06e86a0c130e733a5
Vladimir Louis <vovangus1235@gmail.com>
2021-04-29 22:14:52 +0300

    If you tap on an item, it opens a new activity and if you press button to delete and it's gonna delete the card and close the new activity

--- theapp/src/main/java/com/example/theapp/CardTest.java
+++ theapp/src/main/java/com/example/theapp/CardTest.java
@@ -3,11 +3,15 @@ package com.example.theapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.RemoteController;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.theapp.data.Card;
+import com.example.theapp.data.DatabaseHelper;
 
 public class CardTest extends AppCompatActivity {
 
@@ -16,6 +20,7 @@ public class CardTest extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.card_test);
 
+        DatabaseHelper databaseHelper = new DatabaseHelper(getBaseContext());
 
         Intent intent = getIntent();
         intent.getSerializableExtra("Foreign");
@@ -24,6 +29,15 @@ public class CardTest extends AppCompatActivity {
                 intent.getSerializableExtra("Translation").toString(),
                 intent.getSerializableExtra("Sentence").toString()
         );
+
+        Button button = findViewById(R.id.deleteCard);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                databaseHelper.deleteOne(card);
+                finish();
+            }
+        });
 
         TextView textView = findViewById(R.id.textView2);
         Toast.makeText(getBaseContext(), card.toString(), Toast.LENGTH_LONG).show();
